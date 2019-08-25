400f5e6c76e4b0476d92b1cdaaeecbacf73fa0d6
welisonmenezes <welisonmenezes@hotmail.com>
2019-08-25 16:01:04 -0300

    Add new builded frontend and example of python decorator

--- backend/api/api.py
+++ backend/api/api.py
@@ -4,8 +4,29 @@ from flask_restful import Resource, Api
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 api = Api(api_bp)
 
+# def hasPermission(fn):
+#     def wrapper(*args,**kwargs):
+#         print(args)
+#         if False:
+#             return fn(*args,**kwargs)
+#         else:
+#             return {'message': 'No permission'}
+#     return wrapper
+
+def hasPermission(role):                            
+    def decorator(fn):                                            
+        def decorated(*args,**kwargs): 
+            if (role == 'admin'):                         
+                return fn(*args,**kwargs)
+            else:
+                return {'message': 'No permission'}, 403      
+        return decorated                                          
+    return decorator
+
 class TodoItem(Resource):
+    @hasPermission('user')
     def get(self):
+        print('xxx')
         return {'task': 'get'}
 
     def post(self):
