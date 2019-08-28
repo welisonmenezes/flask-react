f4693d137ef139886b66d23283d6bf87bd850d0d
welisonmenezes <welisonmenezes@hotmail.com>
2019-08-28 00:49:57 -0300

    Add example custom add imagem RichEditor

--- frontend/src/components/shared/RichEditor/RichEditor.js
+++ frontend/src/components/shared/RichEditor/RichEditor.js
@@ -6,11 +6,31 @@ import './RichEditor.css';
 
 class RichEditor extends Component {
 
+  /* eslint-disable */
+  self = null;
+
   constructor(props) {
-    super(props)
-    this.state = { text: '' }
-    this.handleChange = this.handleChange.bind(this)
+    super(props);
+    this.quillRef = null;      // Quill instance
+    this.reactQuillRef = null; // ReactQuill component
+    this.state = { text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    self = this;
   }
+
+  componentDidMount() {
+    this.attachQuillRefs()
+  }
+  
+  componentDidUpdate() {
+    this.attachQuillRefs()
+  }
+  
+  attachQuillRefs = () => {
+    if (typeof this.reactQuillRef.getEditor !== 'function') return;
+    this.quillRef = this.reactQuillRef.getEditor();
+  }
+  
 
   toolbarConfig = {
     'container': '#toolbar',
@@ -18,14 +38,7 @@ class RichEditor extends Component {
       'audio': function(value) {
         console.log(value)
         if (value) {
-          var inpval = prompt('Enter the URL');
-          if (inpval) {
-            alert(encodeURIComponent(inpval))
-            var url = 'https://w.soundcloud.com/player/?url=' + encodeURIComponent(inpval) + '&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true'
-            this.quill.format('video', url);
-          } else {
-            this.quill.format('video', false);
-          }
+          self.insertImageIntoEditor(this.quill);
         } else {
           this.quill.format('video', false);
         }
@@ -35,6 +48,37 @@ class RichEditor extends Component {
 
   handleChange(value) {
     this.setState({ text: value });
+  }
+
+  insertImageIntoEditor(editor) {
+    editor.focus();
+    const img = '<img src="https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2018/03/o-que-e-html.png" alt="test" style="width:200;float: none;" />';
+    const replaced = '##$$image$$##';
+    const index = editor.getSelection().index;
+    editor.insertText(index, replaced);
+    let content = editor.root.innerHTML;
+    content = content.replace(replaced, img);
+    //setTimeout(() => {
+      //editor.root.innerHTML = content;
+      editor.clipboard.dangerouslyPasteHTML(content);
+      //this.setState({text: content})
+    //}, 1);
+  }
+
+  addImage() {
+    /*
+    this.quillRef.focus();
+    const img = '<img src="https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2018/03/o-que-e-html.png" alt="test" style="width:200;float: right;" />';
+    const replaced = '##$$image$$##';
+    const index = this.quillRef.getSelection().index;
+    this.quillRef.insertText(index, replaced);
+    const content = this.quillRef.root.innerHTML;
+    const newContent = content.replace(replaced, img);
+    setTimeout(() => {
+      this.quillRef.root.innerHTML = newContent;
+    }, 1);
+    */
+    this.insertImageIntoEditor(this.quillRef);
   }
 
   render() {
@@ -92,7 +136,14 @@ class RichEditor extends Component {
               <button className='ql-clean'></button>
             </span>
           </div>
-          <ReactQuill value={this.state.text} modules={{toolbar: this.toolbarConfig }} onChange={this.handleChange} />
+          <ReactQuill
+            ref={(el) => { this.reactQuillRef = el }}
+            value={this.state.text}
+            modules={{toolbar: this.toolbarConfig }}
+            onChange={this.handleChange} />
+          <div>
+            <button onClick={() => {this.addImage()}}>Add image</button>
+          </div>
       </div>
     );
   }
